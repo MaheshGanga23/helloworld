d839bf0ac334cc46be445f714c3a08144b8dd801
Mahesh Ganga <54658189+MaheshGanga23@users.noreply.github.com>
2019-08-29 11:45:58 +0530

    Update bubblesort.py

--- bubblesort.py
+++ bubblesort.py
@@ -23,8 +23,8 @@ a = []
 for i in range(1, 10):
   a.append(random.randint(0, 60))
   
-#now a is a list of 10 items, between 0 to 10. ex: [28, 6, 2, 32, 42, 17, 31, 40, 36]
-# if it is already sorted, use following code to shuffle the list, 
+#now a is a list of 10 items, between 0 to 60. ex: [28, 6, 2, 32, 42, 17, 31, 40, 36]
+# if array is already sorted, use following code to shuffle the list, 
 #random.shuffle(a)
 
 # now pass the array to above function(bubble_sort)
